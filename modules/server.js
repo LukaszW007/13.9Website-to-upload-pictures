908f4ff9fa123cae216c8c719240af3bab3a9286
LukaszWiszniewski <wisznu07@gmail.com>
2018-01-31 13:52:25 +0100

    Added image upload and show handler

--- modules/server.js
+++ modules/server.js
@@ -3,25 +3,29 @@ var colors = require('colors');
 
 var handlers = require('./handlers.js');
 
-function start(){
-    function onRequest(request, response) {
+function start () {
+    function onRequest (request, response) {
         console.log('Request received');
         console.log('Request: ' + request.url + ' received!');
         response.writeHead(200, {"Content-type": "text/plain; charset=utf-8"});
-        console.log(request.toString(),response.toString());
+        console.log(request.toString(), response.toString());
         switch (request.url) {
             case '/':
             case '/start':
                 handlers.welcome(request, response);
-                console.log(request,response);
+                console.log(request, response);
                 break;
             case '/upload':
                 handlers.upload(request, response);
+                break;
+            case '/show':
+                handlers.show(request, response);
                 break;
             default:
                 handlers.errors(request, response);
         }
     }
+
     http.createServer(onRequest).listen(9000);
     console.log("Started server!".green);
 }
