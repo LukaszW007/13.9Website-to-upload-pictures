af17d909f7d2421dd81e1671a4913e46db9c0e37
LukaszWiszniewski <wisznu07@gmail.com>
2018-01-31 12:53:47 +0100

    Added html file and start request handling

--- modules/server.js
+++ modules/server.js
@@ -1,23 +1,25 @@
 var http = require('http');
 var colors = require('colors');
 
-var handlers = require('handlers');
+var handlers = require('./handlers.js');
 
 function start(){
     function onRequest(request, response) {
         console.log('Request received');
         console.log('Request: ' + request.url + ' received!');
-        response.writeHead(200, {"Content-type": "text/plain"});
+        response.writeHead(200, {"Content-type": "text/plain; charset=utf-8"});
+        console.log(request.toString(),response.toString());
         switch (request.url) {
             case '/':
             case '/start':
                 handlers.welcome(request, response);
+                console.log(request,response);
                 break;
             case '/upload':
                 handlers.upload(request, response);
                 break;
             default:
-                handlers.error(request, response);
+                handlers.errors(request, response);
         }
     }
     http.createServer(onRequest).listen(9000);
