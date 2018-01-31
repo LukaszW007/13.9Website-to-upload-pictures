4557c716008d3be98634c817f090390d0f817bc8
LukaszWiszniewski <wisznu07@gmail.com>
2018-01-31 10:50:38 +0100

    Added handlers of start, error and upload

--- modules/server.js
+++ modules/server.js
@@ -1,12 +1,24 @@
 var http = require('http');
 var colors = require('colors');
 
+var handlers = require('handlers');
+
 function start(){
     function onRequest(request, response) {
         console.log('Request received');
-        response.writeHead(200,{"Content-type": "text/plain"});
-        response.write('DONE');
-        response.end();
+        console.log('Request: ' + request.url + ' received!');
+        response.writeHead(200, {"Content-type": "text/plain"});
+        switch (request.url) {
+            case '/':
+            case '/start':
+                handlers.welcome(request, response);
+                break;
+            case '/upload':
+                handlers.upload(request, response);
+                break;
+            default:
+                handlers.error(request, response);
+        }
     }
     http.createServer(onRequest).listen(9000);
     console.log("Started server!".green);
